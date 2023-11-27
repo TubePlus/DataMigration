4ae0e72980e3cbb6576507f88a99a987a261ca08
kklee0930 <kklee0930@gmail.com>
2023-11-27 16:50:12 +0900

    FIX MainApplication fixed
    main application 어노테이션 수정했습니다.

--- DataMigration/src/main/java/com/datamigration/datamigration/DataMigrationApplication.java
+++ DataMigration/src/main/java/com/datamigration/datamigration/DataMigrationApplication.java
@@ -3,11 +3,12 @@ package com.datamigration.datamigration;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka // kafka 활성화
-//@EnableScheduling // 스케줄링 활성화
 @EnableJpaAuditing // base entity 자동 적용
 @EnableScheduling // 스케줄링 활성화
 @SpringBootApplication
